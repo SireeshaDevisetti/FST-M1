3b5cc5280e7cc4610d33e2047211a95d5895e362
SireeshaDevisetti <154403153+SireeshaDevisetti@users.noreply.github.com>
2024-03-19 11:45:25 +0530

    Add files via upload

---
+++ Python/Activities/Activity10.py
@@ -0,0 +1,9 @@
+# Given tuple
+num_tuple = (10, 20, 35, 46, 55,60)
+print("Given list is ", num_tuple)
+
+# Print elements that are divisible by 5
+print("Elements that are divisible by 5:")
+for num in num_tuple:
+    if (num % 5 == 0):
+        print(num)
